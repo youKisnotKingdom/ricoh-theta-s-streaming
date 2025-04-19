125dc3e8e236af9d3e0abff06905fed5a1fa7cb3
youKisnotKingdom <nyu24mataka@gmail.com>
2025-04-19 18:24:58 +0900

    fix mjpeg_stream.sh

--- mjpeg_stream.sh
+++ mjpeg_stream.sh
@@ -1,13 +1,14 @@
 #!/usr/bin/env bash
-# gst_theta_mjpeg_stream.sh
 # USAGE: ./gst_theta_mjpeg_stream.sh <HOST_IP> <PORT>
-# ?: ./gst_theta_mjpeg_stream.sh 192.168.86.46 1234
 
-HOST=$1              # ??? IP (??)
-PORT=$2              # ??? UDP ??? (??)
+HOST=$1              # ?: 192.168.86.46
+PORT=$2              # ?: 1234
+
+# libv4l2 ???? MJPEG ????????????????????
+export GST_V4L2_USE_LIBV4L2=1
 
 gst-launch-1.0 -e \
     v4l2src device=/dev/video0 ! \
-    image/jpeg,width=1280,height=720,framerate=30/1 ! \
+    image/jpeg,width=1280,height=720,framerate=2997/200 ! \
     rtpjpegpay pt=96 ! \
     udpsink host=${HOST} port=${PORT} sync=false
